c61f70a072f6a7c5cac1599ea820c0ad38b68f35
Riley McAdoo <riley.mcadoo@gmail.com>
2020-11-20 11:12:27 -0800

    Add more to main js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,37 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// usually when is more things to export import {Triangle} from './triangle.js'
+import CurrencyExchange from './currencyexchange';
+
+function getCurrencyExchange(response)  {
+  if (response.result === "Success")  {
+    console.log("Success");
+    $("#result").html(`Your currency in JPY is: ${response.conversion_rates.JPY}`);
+  } else {
+    console.log(response);
+    $("#result1").html(`there was an erro: ${response['error-type']}`);
+  }
+}
+
+
+$(document).ready(function () {
+  $('#currencyExchange')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 $(document).ready(function() {
   $('#TODO').submit(function(event) {
