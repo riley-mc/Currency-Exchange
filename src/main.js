3fb26c7105b1e957671430e1b890e2a4ef3bca1d
Riley McAdoo <riley.mcadoo@gmail.com>
2020-11-20 13:57:22 -0800

    Add html and fixed js?

--- src/main.js
+++ src/main.js
@@ -4,20 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { CurrencyExchange } from './js/currencyexchange.js';
 
-function getCurrencyExchange(response)  {
+function getCurrencyExchange(response, conversionCurrency, usdCurrency)  {
   if (response.result === "Success")  {
-    console.log("Success");
+    let conversionNumber = response.conversion_rates[conversionCurrency];
     $("#result").html(`Your currency in JPY is: ${response.conversion_rates.JPY}`);
   } else {
-    console.log(response);
-    $("#result1").html(`there was an erro: ${response['error-type']}`);
+    $("#result1").html(`there was an error: ${response['error-type']}`);
   }
 }
 
 
-$(document).ready(function () {
-  $('#currencyExchange')
+async function apiCall(conversionCurrency, usdCurrency) {
+  const response = await Currency.getCurrencyExchange();
+  getCurrencyExchange(response, conversionCurrency, usdCurrency);
 }
 
-
-
+$("#submit").submit(function(event)) {
+  event.preventDefault();
+  const usdCurrency = $("#usd").val();
+  const currency = $("#convertTo").val();
+  apiCall(currency, usdCurrency);
+  $("#currency").html(currency);
+});
